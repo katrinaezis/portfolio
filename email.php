42c2d76da48561abba7614b5e85b89d54d1f329a
Katrina Ezis <katrinaezis@gmail.com>
2015-11-27 08:19:50 -0800

    final

--- email.php
+++ email.php
@@ -1,21 +1,31 @@
+<!-- 
+    Katrina Ezis
+    Info-343
+    Personal Portfolio
+    November 23, 2015
+
+    PHP file to send an email to myself when
+    a user fills out my form. -->
+
 <?php 
 if(isset($_POST['submit'])){
-    $to = "katrinaezis@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
+
+    // My email
+    $to = "katrinaezis@gmail.com";
+    $from = $_POST['email'];
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
     $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['textarea1'];
     $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['textarea1'];
-
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
+
+    // Sending the email
     mail($to,$subject,$message,$headers);
+
+    // Redirecting users to a thank you screen
     header('Location: http://localhost:8080/#/thankyou');
-    // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    //echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    // You cannot use header and echo together. It's one or the other.
     }
 ?>
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,13 +1,28 @@
+// 	Katrina Ezis
+// Info-343
+// Personal Portfolio
+// November 23, 2015
+// This is the JavaScript file for my personal
+// portfolio. It utilizes angular to create a
+// multi-page architecture that is dynamic. I also
+// read in a json data and display pictures with it.
+
 "use strict";
 
+// Initiate angular myApp with a router and infinite-scroll
+// The router will be used for our multi-page architecture
+// The infinite-scroll will be used for an animation effect
 var myApp = angular.module('myApp', ['ui.router', 'infinite-scroll']);
 
+// Configure each of our pages so that they are connected
+// Bring in stateProvider which tells angular where we currently
+// are in terms of navigation and allows us to set our controllers
+// for those different states.
 myApp.config(function($stateProvider) {
 	$stateProvider
 		.state('home', {
 			url: '/home',
 			templateUrl: 'templates/home.html',
-			// template: $templateCache.get('home.html'),
 			controller: 'HomeController'
 		})
 		.state('about', {
@@ -27,39 +42,34 @@ myApp.config(function($stateProvider) {
 		});
 });
 
+// This is our controller for our home page
+// we take in our scope, which will only be applied to this
+// page and $http so that we can read in our json file.
 myApp.controller('HomeController', function($scope, $http){
-	console.log("sup");
+	
+	// Reading json file, when successful we set a scope
+	// value so it has access to json data
 	$http.get('data/data.json').success(function(data){
       console.log(data);
       $scope.projects = data;
     });
 
+    // We are checking if hovering is true or false and 
+    // returning the correct image choice depending on
+    // the selection.
     $scope.getProjectImage = function(project, hovering) {
     	return hovering
     		? 'images/' + project['logo']
     		: 'images/' + project['logo-grey'];
     };
 
-    $scope.projectClick = function(project) {
+    // $scope.projectClick = function(project) {
+    // }
 
-    }
-
+    // Applying annimations to every other image so that
+    // they appear to come into the page towards each other
     $scope.animateProjects = function() {
     	$(".eachProject:odd").addClass("animated fadeInRightBig");
     	$(".eachProject:even").addClass("animated fadeInLeftBig");
     }
-});
-
-// About page controller: define $scope.about as a string
-myApp.controller('AboutController', function($scope){
-	$scope.about = 'string';
-});
-
-// Content controller: define $scope.url as an image
-myApp.controller('ContactController', function($scope){
-	
-});
-
-myApp.controller('ThankyouController', function($scope){
-
 });
